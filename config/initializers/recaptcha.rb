e77765773a883efa985d80aac74c0aa7e625496f
cur1osity <marcin@cur1osity.com>
2017-06-20 23:05:37 +0200

    walcze z captcha

--- config/initializers/recaptcha.rb
+++ config/initializers/recaptcha.rb
@@ -2,13 +2,14 @@ if Rails.env.production?
 
 Recaptcha.configure do |config|
  config.site_key = ENV['site_key']
+ config.secret_key = ENV['secret_key']
   end
 
 else
 
 Recaptcha.configure do |config|
   config.site_key  = '6LcKMyYUAAAAAGfXJba8V7f98UIH6Kt1RWszGGXm'
-
+  config.secret_key = '6LcKMyYUAAAAANpCW1n6eoJCUEebgsOTKlHjYFbk'
 end
 
  end
