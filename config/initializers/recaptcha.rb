0f7a4280aa83eac31faf18307a524e58b73c706a
cur1osity <marcin@cur1osity.com>
2017-06-20 22:43:21 +0200

    zmiany w recaptchy

--- config/initializers/recaptcha.rb
+++ config/initializers/recaptcha.rb
@@ -1,8 +1,15 @@
 if Rails.env.production?
 
 Recaptcha.configure do |config|
- config.site_key = ENV['site_key'],
- config.secret_key = ENV['secret_key']
+ config.site_key = ENV['site_key']
   end
+
+else
+
+Recaptcha.configure do |config|
+  config.site_key  = '6LcKMyYUAAAAAGfXJba8V7f98UIH6Kt1RWszGGXm'
+
+end
+
  end
 
